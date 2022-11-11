31f1782c22048552a1a6da87257549fd1f599e40
XhemaliDurguti <xdurguti7@gmail.com>
2022-11-11 20:08:19 +0100

    Create Project with html and styling Header

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Portfolio Website</title>
+    <!-- css -->
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Header design -->
+    <header>
+        <a href="#" class="logo">Xh <span>e</span>ma</a>
+
+        <ul class="navlist">
+            <li><a href="#home" class="active">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact Me</a></li>
+        </ul>
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </header>
+    <!-- Home Section desig -->
+    <section class="home" id="home">
+        <div class="home-text">
+            <div class="slide">
+                <span class="one">Hello</span>
+                <span class="two">I`m</span>
+            </div>
+            <h1>Xhemali</h1>
+            <h3>Web <span>Developer</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam doloribus a quasi eum.</p>
+            <a href="#" class="btn">Say Hi</a>
+            <a href="#" class="btn2"><span><i class="bx bx-play"></i></span>Watch My Work</a>
+        </div>
+    </section>
+    <!-- custom js link-->
+    <script type="text/javascript" src="./assets/js/script.js></script>
+</body>
+
+</html>
