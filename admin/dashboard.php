3580d0a9e43187411c0b45dbc9b3e20d77449be1
XhemaliDurguti <xdurguti7@gmail.com>
2022-11-14 01:28:00 +0100

    Add Table and view Button

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,35 +1,54 @@
-<?php include './includes/header.php';?>
-    <div class="main-content">
-        <div class="wrapper">
-            <div class="title">
-                <strong>Dashboard</strong>
-            </div>
-
-            <div class="column">
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br />
-                    Categories
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br />
-                    Project
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br />
-                    Services
-                </div>
-
-                <div class="col-4 text-center">
-                    <h1>5</h1>
-                    <br />
-                    Images
-                </div>
-            </div>
+<?php include './includes/header.php'; ?>
+<div class="main-content">
+    <div class="wrapper">
+        <div class="title">
+            <strong>Dashboard</strong>
         </div>
 
+        <div class="column">
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Project
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Services
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Images
+            </div>
+        </div>
+        <table class="tbl_full">
+            <tr>
+                <th>#</th>
+                <th>Title of Project</th>
+                <th>Description</th>
+                <th>Image</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>
+                    <a href="#" class="btn-primary">View</a>
+                </td>
+            </tr>
+        </table>
     </div>
+
+</div>
+<?php include './includes/footer.php'; ?>
